a65e6e1046d577365e4cc902d4d67053cc9a0df1
Gillian Stravers <gillianstravers@icloud.com>
2024-11-21 13:35:55 +0100

    commit

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_resource_group" "this" {
+  count    = var.create_resource_group ? 1 : 0
   name     = var.resource_group.name
   location = var.resource_group.location
   tags = merge(
@@ -10,12 +11,12 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_ip_group" "this" {
-  for_each            = var.ipgroups
-  name                = var.ipgroups.name
-  location            = var.resource_group.location
-  resource_group_name = azurerm_resource_group.this.name
+  for_each = var.ipgroups
 
-  cidrs = var.ipgroups.cidrs
+  name                = each.value.name
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
+  cidrs               = each.value.cidrs
 
   tags = merge(
     try(var.tags),
--- variables.tf
+++ variables.tf
@@ -1,16 +1,28 @@
+variable "create_resource_group" {
+  description = "A flag to create the Resource Group"
+  type        = bool
+  default     = true
+}
+
 variable "resource_group" {
   description = "The Resource Group to create"
-  type        = object({
+  type = object({
     name     = string
     location = string
   })
 }
 
+variable "tags" {
+  description = "A map of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
+
 variable "ipgroups" {
   description = "A map of IP Groups to create"
-  type        = map(object({
+  type = map(object({
     name  = string
     cidrs = list(string)
-  })
+    })
   )
 }
